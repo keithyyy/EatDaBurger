57bde56ad3780ca12f4281d112ac13440f422614
Keith Crooc <keith.crooc@gmail.com>
2021-01-13 21:02:54 -0500

    created routes and in the controllers folder. no Delete function yet

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const router = express.Router();
+
+// import burger model
+const burger = require('../models/burger.js');
+
+// creating routes
+router.get('/', (req,res) => {
+    burger.selectAll((data) => {
+        const hbsObject = {
+            burgers: data,
+        };
+        console.log('hbsObject: ', hbsObject);
+        res.render('index', hbsObject)
+    })
+})
+
+router.post('/api/burgers', (req,res) => {
+    cat.create(['burger_name', 'devoured'], [req.body.name, req.body.devoured], (result) => {
+        // send back id of the new burger
+        res.json({ id: result.insertId });
+    })
+})
+
+router.put('/api.burgers/:id', (req, res) =>{
+    const condition = `id = ${req.params.id}`;
+
+    console.log('condition', condition);
+
+    burger.updateOne(
+        {
+            devoured: req.body.devoured,
+        },
+        condition, 
+        (result) => {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end()
+        }
+
+    )
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give server access to them
-const routes = require("./controllers/burgerController.js");
+const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
